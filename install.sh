10af04d8c4db772dab466a8fff167b51b336b238
dj1vs <icemcpe099@gmail.com>
2024-07-07 22:25:00 +0300

    add i3/config

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ ln -s "$PWD/kitty.conf" ~/.config/kitty
 mkdir ~/.config/polybar
 ln -s "$PWD/polybar/config.ini" ~/.config/polybar
 ln -s "$PWD/polybar/launch.sh"  ~/.config/polybar
+
+mkdir ~/.config/i3
+ln -s "$PWD/i3/config" ~/.config/i3
